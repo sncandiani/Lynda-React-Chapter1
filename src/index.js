8ebf599db9021366e041c8684b7e8d41c9728d7f
sncandiani <sofiancandiani@gmail.com>
2020-08-25 21:24:12 -0500

    creating initial routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// 1. createElement is another way to create elements in React
+  // ReactDOM.render(
+  //   React.createElement("h1", {style: {color: "red"}}, "Hello"),
+  //   document.getElementById('root')
+  // ); 
+
+// 2.  Babel and React work together to make code more readable
+
+// ReactDOM.render(
+//   <ul>
+//     <li>Donuts</li>
+//     <li>Pies</li>
+//   </ul>, 
+//   document.getElementById("root")
+// )
+
+//  3 Variables and React elements 
+
+// const city = {
+//   name: "Nashville",
+//   country: "USA"
+// } 
+
+// ReactDOM.render(
+//   <>
+//   <h1>{city.name}</h1>
+// <h2>{city.country}</h2> </>, 
+// document.getElementById("root")
+// ); 
+
+//  4 React Components : Collections of React elements that we can use to build
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hi!</h1>
+      <p>Welcome to React!</p>
+    </div>
+  )
+} 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <Hello />, 
+  document.getElementById("root")
+)
